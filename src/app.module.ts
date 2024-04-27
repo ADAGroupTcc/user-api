3952f86d51ec40cfcf5cc4da87b7b3f444c9632e
RafalFDS2 <rafael.ferraz@picpay.com>
2024-04-27 02:05:54 -0300

    auth method

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { HealthController } from './health/health.controller';
 import { UsersModule } from './users/users.module';
 
+const MONGO_CERT = require('../mongodb_cert.pem');
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     UsersModule,
-    MongooseModule.forRoot(process.env.DATABASE_URL),
+    MongooseModule.forRoot(process.env.DATABASE_URL, {
+      authMechanism: 'MONGODB-X509',
+      cert: MONGO_CERT
+    }),
   ],
   controllers: [HealthController],
 })
