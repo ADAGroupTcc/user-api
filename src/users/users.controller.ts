b19770eb08085fd78bfd55d0e07bc3d9fdedda3b
RafalFDS2 <rafael.ferraz@picpay.com>
2024-04-27 23:46:39 -0300

    feat: implementation of list users with all query param needed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UserDto } from './dto';
 import { UsersService } from './users.service';
 
@@ -9,5 +9,20 @@ export class UsersController {
   @Post()
   async createUsers(@Body() user: UserDto) {
     return await this.userService.create(user);
+  }
+
+  @Get()
+  async listUsers(@Query('cpf') cpf: string, @Query('limit') limit: number, @Query('page') page: number) {
+    if (!limit) {
+      limit = 10;
+    }
+    if (!page) {
+      page = 1;
+    }
+    var users = await this.userService.list(cpf, limit, page);
+    return {
+      users,
+      next: page
+    }
   }
 }
