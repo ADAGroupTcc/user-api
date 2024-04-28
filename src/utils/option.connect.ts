563a0ed0743d1c963cd7322ba4f1c5b6a9012cc2
RafalFDS2 <rafael.ferraz@picpay.com>
2024-04-28 12:32:17 -0300

    FINALMENTE

--- src/utils/option.connect.ts
+++ src/utils/option.connect.ts
@@ -6,15 +6,12 @@ export const localDbOptions: MongooseModuleOptions = {
   retryDelay: Number(process.env.RETRY_DELAY),
 }
 export const serverDbOptions: MongooseModuleOptions = {
-  appName: 'adda-cluster',
-  authMechanism: 'MONGODB-X509',
-  authSource: '$external',
-  retryWrites: true,
-  w: 'majority',
-  tls: true,
-  cert: process.env.CERT,
-  key: process.env.KEY,
-  serverApi: ServerApiVersion.v1,
-  retryAttempts: Number(process.env.RETRY_ATTEMPTS),
-  retryDelay: Number(process.env.RETRY_DELAY),
+  auth: {
+    username: process.env.MONGO_USERNAME,
+    password: process.env.MONGO_PASSWORD,
+  },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+  }
 }
