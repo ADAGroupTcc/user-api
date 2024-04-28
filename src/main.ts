e9b908f7dddc4abfa4b526f5fd36e50e037466b0
RafalFDS2 <rafael.ferraz@picpay.com>
2024-04-28 10:59:06 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter()
   );
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(process.env.PORT, process.env.HOSTS_AVAILABLE);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
